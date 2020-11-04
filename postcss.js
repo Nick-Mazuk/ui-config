02a50639eac464ca24c03d18967b5e72911f1af3
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2020-11-03 16:34:15 -0800

    Added ability to create more global rules

--- postcss.js
+++ postcss.js
@@ -2,27 +2,44 @@ const postcss = require('postcss')
 const flatten = require('flat')
 const colors = require('./colors')
 
+const addColors = (css) => {
+    const rule = postcss.rule({ selector: ':root' })
+    const flattenedColors = flatten(colors, {
+        delimiter: '-',
+        safe: true
+    })
+    const decls = []
+    for (const color in flattenedColors) {
+        if (typeof flattenedColors[color] === 'string') continue;
+        decls.push(postcss.decl({
+            prop: '--c-' + color,
+            value: flattenedColors[color].join(', ')
+        }))
+    }
+    decls.push(postcss.decl({
+        prop: '--c-link',
+        value: 'var(--c-primary-default)'
+    }))
+    css.prepend(rule.append(decls))
+}
+
+const addRule = (css, selector, prop, value) => {
+    const rule = postcss.rule({ selector: selector })
+    css.prepend(rule.append([postcss.decl({
+        prop: prop,
+        value: value
+    })]))
+}
+
+const addRules = (css) => {
+    addRule(css, '.ql-editor', 'outline', 'none')
+}
+
 const fn = (vars = {}) => {
     return (css) => {
         if (!css.source.input.file.includes('module')) {
-            const rule = postcss.rule({ selector: ':root' })
-            const flattenedColors = flatten(colors, {
-                delimiter: '-',
-                safe: true
-            })
-            const decls = []
-            for (const color in flattenedColors) {
-                if (typeof flattenedColors[color] === 'string') continue;
-                decls.push(postcss.decl({
-                    prop: '--c-' + color,
-                    value: flattenedColors[color].join(', ')
-                }))
-            }
-            decls.push(postcss.decl({
-                prop: '--c-link',
-                value: 'var(--c-primary-default)'
-            }))
-            css.prepend(rule.append(decls))
+            addColors(css)
+            addRules(css)
         }
     }
 }
