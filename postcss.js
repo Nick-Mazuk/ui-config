48176330b0bc31308cd90eb6966ed9e22a2079ce
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2020-11-27 09:53:51 -0800

    Undoing the postcss fix for default styles

--- postcss.js
+++ postcss.js
@@ -12,7 +12,7 @@ const addColors = (css) => {
     for (const color in flattenedColors) {
         if (typeof flattenedColors[color] === 'string') continue;
         decls.push(postcss.decl({
-            prop: '--c-' + color.toLowerCase(),
+            prop: '--c-' + color,
             value: flattenedColors[color].join(', ')
         }))
     }
