88927c5d89048fd94e172b3cdcb4b779c3fb5fca
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-04-02 16:30:13 -0700

    Remove useless script

--- src/build-scripts/index.ts
+++ src/build-scripts/index.ts
@@ -3,11 +3,9 @@ import fs from 'fs-extra'
 import { createColorTypes } from './create-color-types'
 import { createColorsCss } from './create-colors-css'
 import { createCss } from './create-css'
-import { createTailwindConfig } from './create-tailwind-config'
 
 fs.ensureDirSync('lib')
 
 createColorsCss()
 createColorTypes()
 createCss()
-createTailwindConfig()
