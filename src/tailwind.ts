177d7e35a36fd7ca5438d6d61041741146e95ef2
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-04-25 15:25:56 -0700

    Update font size

--- src/tailwind.ts
+++ src/tailwind.ts
@@ -23,6 +23,22 @@ export const config = {
             full: '100%',
             ...theme('spacing'),
         }),
+        fontSize: {
+            xs: ['0.625rem', { lineHeight: '0.875rem' }],
+            sm: ['0.75rem', { lineHeight: '1rem' }],
+            base: ['0.875rem', { lineHeight: '1.25rem' }],
+            lg: ['1rem', { lineHeight: '1.5rem' }],
+            xl: ['1.125rem', { lineHeight: '1.75rem' }],
+            '2xl': ['1.25rem', { lineHeight: '1.75rem' }],
+            '3xl': ['1.5rem', { lineHeight: '2rem' }],
+            '4xl': ['1.875rem', { lineHeight: '2.25rem' }],
+            '5xl': ['2.25rem', { lineHeight: '2.5rem' }],
+            '6xl': ['3rem', { lineHeight: '1' }],
+            '7xl': ['3.75rem', { lineHeight: '1' }],
+            '8xl': ['4.5rem', { lineHeight: '1' }],
+            '9xl': ['6rem', { lineHeight: '1' }],
+            '10xl': ['8rem', { lineHeight: '1' }],
+        },
         fontWeight: {
             thin: 'calc(100 * var(--font-weight-multiplier, 1))',
             extralight: 'calc(200 * var(--font-weight-multiplier, 1))',
@@ -61,9 +77,6 @@ export const config = {
                 display: ['Gilroy', 'Inter', ...defaultTheme.fontFamily.sans],
                 mono: ['SFMono-Regular', ...defaultTheme.fontFamily.mono],
             },
-            borderColor: (theme) => ({
-                DEFAULT: theme('colors.primary.500'),
-            }),
             maxWidth: {
                 '1/4': '25%',
                 '1/2': '50%',
